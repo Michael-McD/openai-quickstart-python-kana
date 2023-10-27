89cc3e1e8631db3a4d18c48c69f6b54a4cfbc539
Michael-McD <michael.mcdowell@hotmail.co.uk>
2023-10-27 12:44:33 +0100

    Not using Async!

--- app.py
+++ app.py
@@ -1,35 +1,33 @@
 import os
 
-import openai
-from flask import Flask, redirect, render_template, request, url_for
+from openai import OpenAI
+from flask import Flask, redirect, render_template, request, url_for, json
 
 app = Flask(__name__)
-openai.api_key = os.getenv("OPENAI_API_KEY")
+client = OpenAI()
 
-
-@app.route("/", methods=("GET", "POST"))
+@app.route("/", methods=["GET","POST"])
 def index():
     if request.method == "POST":
-        animal = request.form["animal"]
-        response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=generate_prompt(animal),
-            temperature=0.6,
-        )
-        return redirect(url_for("index", result=response.choices[0].text))
+        prompt = request.form["prompt"]
+        message = generate_prompt(prompt)
+        completion = client.chat.completions.create(
+            model="gpt-3.5-turbo", 
+            messages=message)
+        
+        return redirect(url_for("index", result=completion.choices[0].message.content))
 
     result = request.args.get("result")
     return render_template("index.html", result=result)
 
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate, public, max-age=0"
+    response.headers["Expires"] = '0'
+    response.headers["Pragma"] = "no-cache"
+    return response
 
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
+# completion = await client.chat.completions.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": "Hello world"}])
 
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
-    )
+def generate_prompt(prompt):
+    return [{"role": "user", "content": "Translate the following using only hiragana / katakana:{}".format(prompt.capitalize())}]
